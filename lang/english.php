8592e4bd29f3d60fb4a33c18014188e70ac7e164
Daniel Pietersen [Desktop] <Skowt@users.noreply.github.com>
2015-01-08 12:39:54 +0200

    Moved from BitBucket to GitHub
    Yeah!

---
+++ lang/english.php
@@ -0,0 +1,3 @@
+<?php
+
+$_ADDONLANG['moduledropdown'] = "All functionality needed for this module can be configured on the 'Setup -> Addon Modules' page.";
--- paypalsub_reminder/lang/english.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$_ADDONLANG['moduledropdown'] = "All functionality needed for this module can be configured on the 'Setup -> Addon Modules' page.";
